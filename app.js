aa1155bcb28592dd07856154bd87b3525d721ac4
Mykon <helvis89@gmail.com>
2023-05-02 17:58:34 -0300

    inserir e deletar favoritos

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 const express = require('express');
 const rotaLivro = require('./rotas/livro');
-<<<<<<< HEAD
-
-const app = express();
-app.use(express.json());
-
-app.use('/livros', rotaLivro);
-=======
 const rotaFavorito = require('./rotas/favorito');
 const cors = require('cors');
 
@@ -16,7 +9,6 @@ app.use(cors({ origin: '*' }));
 
 app.use('/livros', rotaLivro);
 app.use('/favoritos', rotaFavorito);
->>>>>>> ab9ddc73ed3498f19449df9c22b7290660573acf
 
 const port = 8000;
 
